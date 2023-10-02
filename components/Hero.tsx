d3706f7f4f92d35881724c88136b0ca6ca2e0ac4
Jay Hogan <jayhogan.dev@gmail.com>
2023-10-01 20:51:52 -0700

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,11 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const Hero = () => {
   return (
     <section className="flex w-full h-[800px] bg-blue-400">
-        <div className="flex relative w-[300px] h-[400px]">
-
-        <Image src="/hero.png" alt="personal trainer" fill /> 
-        </div>
+      <h1>Home</h1>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
