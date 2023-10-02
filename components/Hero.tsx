9573f1b5001f6185454d9616df167d7f187a32f2
Jay Hogan <jayhogan.dev@gmail.com>
2023-10-02 07:17:01 -0700

    hero image included

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="flex w-full h-[800px] bg-blue-400">
-      <h1>Home</h1>
+    <section className="flex w-full h-[800px] bg-[url('/hero.png')] bg-cover bg-center bg-no-repeat">
+      <div className="flex flex-col md:flex-row md:justify-between items-center h-[800px] w-full">
+        <div className="bg-red-300">
+          <h1>Home</h1>
+        </div>
+        <div className="bg-blue-300">
+          <h1>Right Side</h1>
+        </div>
+      </div>
     </section>
   );
 };
