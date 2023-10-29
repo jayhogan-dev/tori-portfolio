684dfdf7fa36a0a026b4ecdd5692e02739a4a5ad
Jay Hogan <jayhogan.dev@gmail.com>
2023-10-29 11:36:30 -0700

    disabled eslint for shadcn

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,4 @@
-// import Link from "next/link";
 import LightContainer from "./containers/Light";
-
-// SVGR
-// import LinkedIn from "@/public/social-icons/linkedin.svg";
-// import Github from "@/public/social-icons/github.svg";
 
 const Footer = () => {
   return (
@@ -12,26 +7,6 @@ const Footer = () => {
         <p className="mb-4 text-body-regular text-primary-black-400 dark:text-primary-white-800">
           &copy; 2023 Tori. All rights reserved
         </p>
-        <div className="flex items-center justify-center gap-5">
-          <p>Add any Socials here</p>
-          {/* <p>Built By: </p>
-          <Link href="https://www.jayhogandev.com" target="_blank">
-            Jay Hogan
-          </Link> */}
-          {/* <Link
-            href="https://www.linkedin.com/in/jay-hogan-66610b90/"
-            target="_blank"
-          >
-            <LinkedIn className="fill-primary-black-400 dark:fill-white" />
-          </Link>
-          <Link
-            href="https://github.com/jayhogan-dev"
-            target="_blank"
-            className="relative h-6 w-6"
-          >
-            <Github className="fill-primary-black-400 dark:fill-white" />
-          </Link> */}
-        </div>
       </div>
     </LightContainer>
   );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
